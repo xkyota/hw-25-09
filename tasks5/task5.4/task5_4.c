d85929f96b090354a3e3c5f8730d18bb7e6add4e
Maksym Durikhin <max1mkasss.mail@gmail.com>
2025-09-26 10:05:53 +0300

    1.4 task5.4 is done

---
+++ tasks5/task5.4/task5_4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int str_eq(const char *a, const char *b) {
+    while (*a && *b) {
+        if (*a != *b) return 0;
+        a++; b++;
+    }
+    return *a == *b;
+}
+
+int main(int argc, char *argv[]) {
+    int port = 8080;
+    char host[256] = "localhost";
+    int debug = 0;
+
+    for (int i = 1; i < argc; i++) {
+        if ((str_eq(argv[i], "--port") || str_eq(argv[i], "-p")) && i + 1 < argc) {
+            port = atoi(argv[++i]);
+        } else if ((str_eq(argv[i], "--host") || str_eq(argv[i], "-h")) && i + 1 < argc) {
+            int j = 0;
+            char *src = argv[++i];
+            while (src[j] && j < 255) {
+                host[j] = src[j];
+                j++;
+            }
+            host[j] = '\0';
+        } else if (str_eq(argv[i], "--debug") || str_eq(argv[i], "-d")) {
+            debug = 1;
+        }
+    }
+
+    printf("Host: %s\n", host);
+    printf("Port: %d\n", port);
+    if (debug) {
+        printf("Debug: true\n");
+    } else {
+        printf("Debug: false\n");
+    }
+
+    return 0;
+}
