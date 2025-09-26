9736e7217a88882b52e686abb94c97672c08e545
Maksym Durikhin <max1mkasss.mail@gmail.com>
2025-09-26 09:49:37 +0300

    1.3 task5.3 is done

---
+++ tasks5/task5.3/task5_3.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int str_eq(const char *a, const char *b) {
+    while (*a && *b) {
+        if (*a != *b) return 0;
+        a++; b++;
+    }
+    return *a == *b;
+}
+
+int main(int argc, char *argv[]) {
+    int verbose = 0;
+    int number = 0;
+    int number_set = 0;
+    char *files[argc];
+    int file_count = 0;
+
+    for (int i = 1; i < argc; i++) {
+        if (str_eq(argv[i], "-v")) {
+            verbose = 1;
+        } else if (str_eq(argv[i], "-n")) {
+            if (i + 1 < argc) {
+                number = atoi(argv[i + 1]);
+                number_set = 1;
+                i++; 
+            } else {
+                printf("Error: -n flag requires a number\n");
+                return 1;
+            }
+        } else if (argv[i][0] == '-') {
+            printf("Error: Unknown flag %s\n", argv[i]);
+            return 1;
+        } else {
+            files[file_count++] = argv[i];
+        }
+    }
+
+    printf("Verbose: %s\n", verbose ? "ON" : "OFF");
+    if (number_set)
+        printf("Number: %d\n", number);
+    printf("Files:\n");
+    for (int i = 0; i < file_count; i++) {
+        printf("  %s\n", files[i]);
+    }
+
+    return 0;
+}
+
+//! With Copilot assistance 
