5c0d71fb493b72959241fb68859d9c0aa00032d6
Maksym Durikhin <max1mkasss.mail@gmail.com>
2025-09-26 09:37:58 +0300

    1.2 task5.2 is done

---
+++ tasks5/task5.2/task5_2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int str_eq(const char *a, const char *b) {
+    while (*a && *b) {
+        if (*a != *b) return 0;
+        a++; b++;
+    }
+    return *a == *b;
+}
+
+int main(int argc, char *argv[]) {
+    int number = 0;
+    char *output = NULL;
+    int verbose = 0;
+    char *input = NULL;
+
+    for (int i = 1; i < argc; i++) {
+        if (str_eq(argv[i], "-n") && i + 1 < argc) {
+            number = atoi(argv[++i]);
+        } else if (str_eq(argv[i], "-o") && i + 1 < argc) {
+            output = argv[++i];
+        } else if (str_eq(argv[i], "-v")) {
+            verbose = 1;
+        } else if (argv[i][0] != '-') {
+            input = argv[i];
+        }
+    }
+
+    printf("Number: %d\n", number);
+    if (output)
+        printf("Output: %s\n", output);
+    else
+        printf("Output: (none)\n");
+
+    if (verbose)
+        printf("Verbose: yes\n");
+    else
+        printf("Verbose: no\n");
+
+    if (input)
+        printf("Input: %s\n", input);
+    else
+        printf("Input: (none)\n");
+
+    return 0;
+}
