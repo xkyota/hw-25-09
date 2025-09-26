94b91030b88feda46a05e1c7c96cb322d78c04ed
Maksym Durikhin <max1mkasss.mail@gmail.com>
2025-09-26 08:46:10 +0300

    0.6 task3.3 is done

---
+++ tasks3/task3.3/task3_3.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int a2i(char *number);
+
+
+int main(int argc, char *argv[])
+{ 
+    if (argc < 2)
+    {
+        printf("Usage: ./program 10 5 20 15 2\n"); 
+        return 1; 
+    }
+
+    int sum = 0;
+    int minimum = a2i(argv[1]);
+    int maximum = a2i(argv[1]);
+    int i;
+
+    for (i = 1; i < argc; i++)
+    {
+        int value = a2i(argv[i]);
+        sum += value;
+        if (value < minimum) minimum = value;
+        if (value > maximum) maximum = value;
+    }
+
+    float average = (float)sum / (argc - 1);
+
+    printf("Sum is %d\n", sum);
+    printf("Average is %.2f\n", average);
+    printf("Minimum is %d\n", minimum);
+    printf("Maximum is %d\n", maximum);
+
+    return 0; 
+}
+
+int a2i(char *number)
+{
+    int result = 0;
+    int i = 0;
+    int sign = 1;
+
+    if (number[0] == '-')
+    {
+        sign = -1;
+        i++;
+    }
+
+    while (number[i] != '\0')
+    {
+        if (number[i] < '0' || number[i] > '9')
+        {
+            break;
+        }
+        result = result * 10 + (number[i] - '0');
+        i++;
+    }
+
+    return sign * result;
+}
